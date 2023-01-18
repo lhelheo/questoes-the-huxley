7c27df31c01bbbe8b6799a2f1b7c0d1a6ee4db39
Leonardo Oliveira Villalva <122951276+lhelheo@users.noreply.github.com>
2023-01-18 00:00:40 -0300

    Update maior multiplo.c

--- maior multiplo.c
+++ maior multiplo.c
@@ -1,3 +1,14 @@
+/* Descrição
+Seu objetivo é determinar o maior múltiplo de um inteiro dado menor do que ou igual a um outro inteiro dado
+
+Formato de entrada
+Consiste de dois inteiros positivos M e N.
+
+Formato de saída
+A saída consiste do maior número que seja múltiplo de M e menor ou igual a N, 
+se não houver um múltiplo de M menor ou igual a N você deve imprimir "sem multiplos menores que N", sem as aspas, 
+onde N deve ser substituído pelo valor de entrada N. */
+
 #include <stdio.h>
 int maximo(int a, int b) {
     return (b/a)*a;
