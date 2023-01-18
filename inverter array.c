667d4321d5e2fd3c9951c4affb3e3fdf3ec225e4
Leonardo Oliveira Villalva <122951276+lhelheo@users.noreply.github.com>
2023-01-18 00:01:14 -0300

    Update inverter array.c

--- inverter array.c
+++ inverter array.c
@@ -1,3 +1,14 @@
+/* Descrição
+Seu programa deve receber um vetor de N valores inteiros e imprimir na ordem inversa.
+
+Formato de entrada
+Na primeira linha de entrada o programa recebe um valor inteiro N de entrada. 
+A segunda linha contém N inteiros separados por espaço contendo os valores do array.
+
+Formato de saída
+Uma linha contendo os números do vetor em ordem invertida separados por espaços em branco, seguida de um final de linha.
+Depois do último número, não deve existir um espaço em branco.*/
+
 #include <stdio.h>
 
 int main(){
