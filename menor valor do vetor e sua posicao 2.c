8881c7b6f3b2b151dd04df7ba8e54a471853f2e9
Leonardo Oliveira Villalva <122951276+lhelheo@users.noreply.github.com>
2023-01-18 00:06:11 -0300

    Update menor valor do vetor e sua posicao 2.c

--- menor valor do vetor e sua posicao 2.c
+++ menor valor do vetor e sua posicao 2.c
@@ -1,3 +1,19 @@
+/* Descrição
+Escrever um programa que lê um número N. Este N é o tamanho de um array.
+
+Em seguida, leia cada um dos N números do array, encontre o menor elemento desse array, a sua posição dentro do array e imprima essas informações.
+Considere que o array começa na posição 0
+
+Formato de entrada
+A primeira linha da entrada contém um inteiro N (1 <N <1000), indicando o número de elementos que devem ser lidos no array de números inteiros. 
+A segunda linha contém cada um dos valores de N, separadas por um espaço.
+Obs: não haverão números repetidos.
+
+Formato de saída
+A primeira linha exibe a mensagem "Menor valor:" seguido por um espaço e o menor número lido na entrada. 
+A segunda linha exibe a mensagem "POSIÇÃO:" seguido por um espaço e a posição do array em que o menor número é encontrado, 
+lembrando que o array começa na posição zero. */
+
 #include <stdio.h>
 
 int main(){
